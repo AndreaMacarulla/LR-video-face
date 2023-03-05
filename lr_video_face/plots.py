5780f298fafcd9baaffbca5043ed3b6647e1d113
“AndreaMacarulla” <andrea.macarulla@gmail.com>
2023-03-05 16:03:15 +0100

    nada

--- lr_video_face/plots.py
+++ lr_video_face/plots.py
@@ -460,8 +460,9 @@ def subplot_new(ax1,results:Dict, cllr_expert):
     color = 'tab:red'
     ax1.set_ylabel('Cllr')
     ax1.set_xlabel('% of discarded pairs', color = color)
-    #ax1.plot('dropout','Cllr', data = df_plot1, color = color, marker = 'o', label= 'Quality based drop')
-    x = ax1.boxplot(df_plot1['Cllr2'])
+    
+    ax1.boxplot(df_plot1['Cllr2'],positions=df_plot1['dropout'],sym='',notch = True,manage_ticks = True, whis= 0)
+    ax1.plot('dropout','Cllr', data = df_plot1, color = color, marker = 'o', label= 'Quality based drop')
 
     
     df_0drop = df_plot1.loc[(df_plot1['dropout'] == 0)]
